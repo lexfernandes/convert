6f0dd9d9b45f1dea7ddc7c814e626f039e7da105
lexfernandes <sr.lexfernandes@gmail.com>
2025-10-23 20:52:31 -0300

    Obtendo o vallor do input e validando o input para receber somente números

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Funcionando");
+
+const amount = document.getElementById("amount");
+
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g;
+  amount.value = amount.value.replace(hasCharactersRegex, "");
+});
