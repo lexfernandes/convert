d9f200c2b0b32e0602b45bc580e2e5c099ba6511
lexfernandes <sr.lexfernandes@gmail.com>
2025-10-27 19:52:19 -0300

    Exibindo o footer

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 console.log("Funcionando");
 
+const USD = 4.87;
+const EUR = 5.32;
+const GBP = 6.08;
+
 // obtendo os elementos do formularios.
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
 
 amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g;
@@ -13,5 +18,29 @@ amount.addEventListener("input", () => {
 // capturando o evento de submit  do formulário.
 form.onsubmit = (event) => {
   event.preventDefault();
-  console.log(currency.value);
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+    default:
+      break;
+  }
 };
+
+//função para converter a moeda.
+function convertCurrency(amount, price, symbol) {
+  try {
+    // Aplica a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result");
+  } catch (error) {
+    // remove a classe do footer ocultando ele da tela.
+    footer.classList.remove("show-result");
+    console.log(error);
+    alert("Não foi possível converter. Tente novamente mais tarde.");
+  }
+}
